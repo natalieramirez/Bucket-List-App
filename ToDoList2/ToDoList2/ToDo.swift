4b68092a37a63c410838f47deccb51a2b960f58d
Natalie Ramirez <60053094+natalieramirez@users.noreply.github.com>
2020-07-01 16:34:12 -0400

    uploaded todo list

---
+++ ToDoList2/ToDoList2/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList2
+//
+//  Created by Natalie Ramirez on 6/22/20.
+//  Copyright © 2020 Natalie Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ ToDoList2/ToDoList2/ToDoTableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  TableViewController.swift
+//  ToDoList2
+//
+//  Created by Natalie Ramirez on 6/22/20.
+//  Copyright © 2020 Natalie Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos : [ToDo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        toDos = createToDos()
+    }
+    
+    func createToDos() -> [ToDo] {
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        //important is set to false by default
+        return [swift, dog]
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+          cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+          cell.textLabel?.text = toDo.name
+        }
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let addVC = segue.destination as? AddToDoViewController {
+        addVC.previousVC = self
+        }
+    }
+}
